2faa0ede646ba202f1c52f03bf79e9c48bfd7be8
غيث عربكرلي <110243339+lvlovalm7bob@users.noreply.github.com>
2024-01-14 15:36:26 +0300

    Fixed api end point to deploy

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,7 @@ const production = false;
 const instance = axios.create({
   withCredentials: true,
   baseURL: production
-    ? "https://watch-party-uvre.onrender.com"
+    ? "https://watch-party-uvre.onrender.com/api"
     : "http://localhost:5000/api",
 });
 
