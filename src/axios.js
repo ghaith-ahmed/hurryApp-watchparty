cfc69a8429d07eb400644e14932410251156e341
Ghaith ahmed <lafalava477@gmail.com>
2024-01-14 16:16:30 +0300

    Final build

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,7 @@ const production = true;
 const instance = axios.create({
   withCredentials: true,
   baseURL: production
-    ? "https://watch-party-uvre.onrender.com"
+    ? "https://watch-party-uvre.onrender.com/api"
     : "http://localhost:5000/api",
 });
 
